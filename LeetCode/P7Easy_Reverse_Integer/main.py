dec75c7eab58c6b34760b3054a4a587facf8e797
keshprad <keshav.r.pradeep@gmail.com>
2020-09-03 10:18:14 -0700

    add lc p7

---
+++ LeetCode/P7Easy_Reverse_Integer/main.py
@@ -0,0 +1,20 @@
+# Problem: https://leetcode.com/problems/reverse-integer/
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        isNeg = x < 0
+
+        x = int(str(abs(x))[::-1])
+        if isNeg:
+            return self.overflow(-x)
+        else:
+            return self.overflow(x)
+
+    def overflow(self, x):
+        minx = -1 * (2 ** 31)
+        maxx = (2 ** 31) - 1
+
+        if x < minx or x > maxx:
+            return 0
+        else:
+            return x
