a1ab569767bb11bce016a3e612b77d13b6db19c0
keshprad <keshav.r.pradeep@gmail.com>
2020-09-02 23:17:38 -0700

    solved leetcode TwoSum

---
+++ LeetCode/Two-Sum/main.py
@@ -0,0 +1,21 @@
+# Problem: https://leetcode.com/problems/two-sum/
+
+
+def main(nums, target):
+    diff = {}
+
+    for i in range(len(nums)):
+        item = nums[i]
+
+        if target - item not in diff:
+            diff[item] = i
+        else:
+            return [diff[target - item], i]
+
+
+if __name__ == '__main__':
+    nums = input("Array of Integers: ").replace(",", " ").split()
+    nums = [int(num) for num in nums]
+    target = int(input("target integer: "))
+
+    print(main(nums, target))
