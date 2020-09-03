7855d153ca01313c24f94652f0868724ca675995
keshprad <keshav.r.pradeep@gmail.com>
2020-09-03 01:03:58 -0700

    Add LeetCode P3

---
+++ LeetCode/P3Medium_Longest_Substring_Without_Repeating_Characters/main.py
@@ -0,0 +1,13 @@
+# Problem: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        used = ''
+        max_len = 0
+
+        for char in s:
+            if char in used:
+                max_len = max(max_len, len(used))
+                used = used[used.index(char) + 1:]
+            used += char
+        return max(max_len, len(used))
