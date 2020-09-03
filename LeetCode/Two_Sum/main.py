4773ad447a7331a49c412eb3aed4091a76a31bd3
keshprad <keshav.r.pradeep@gmail.com>
2020-09-02 23:22:30 -0700

    leetcode format

---
+++ LeetCode/Two_Sum/main.py
@@ -0,0 +1,13 @@
+# Problem: https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        diff = {}
+
+        for i in range(len(nums)):
+            item = nums[i]
+
+            if target - item not in diff:
+                diff[item] = i
+            else:
+                return [diff[target - item], i]
