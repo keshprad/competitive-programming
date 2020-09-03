4d51830ee16c199cf96ddff6d445e5bf933724f4
keshprad <keshav.r.pradeep@gmail.com>
2020-09-03 08:33:07 -0700

    Add LC P5

---
+++ LeetCode/P5Medium_Longest_Palindromic_Substring/main.py
@@ -0,0 +1,14 @@
+# Problem: https://leetcode.com/problems/longest-palindromic-substring/solution/
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        pal = ''
+
+        for l in range(len(s)):
+            for r in range(len(s), l, -1):
+                # Check if the length is longer than the current palindrome
+                # Check if it is a palindrome from index l to r
+                if r - l > len(pal) and s[l:r] == s[l:r][::-1]:
+                    pal = s[l:r]
+                    break
+        return pal
