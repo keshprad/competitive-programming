9a691a07fe0e9163e7f9e55b9f168d6b7b9c9058
keshprad <keshav.r.pradeep@gmail.com>
2020-09-09 11:19:21 -0700

    Coded 266B codeforces Queue at school

---
+++ Codeforces/266B_Queue_at_the_School/main.py
@@ -0,0 +1,21 @@
+# Problem: https://codeforces.com/problemset/problem/266/B
+
+
+def main(queue, t):
+    for j in range(t):
+        # Find indices to switch
+        to_switch = []
+        curr_ind = queue.find("BG", 0)
+        while curr_ind != -1:
+            to_switch.append(curr_ind)
+            curr_ind = queue.find("BG", curr_ind+1, len(queue))
+
+        for i in to_switch:
+            queue = queue[:i] + "GB" + queue[i+2:]
+    return queue
+
+
+if __name__ == '__main__':
+    nt = list(map(int, input().split()))
+    n, t = nt[0], nt[1]
+    print(main(queue, t))
