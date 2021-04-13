0c21f6c3f9e014c4f69aeeba80786ded16bbb20b
keshprad <keshav.r.pradeep@gmail.com>
2021-04-13 08:43:03 -0700

    Complete 3sum closest problem

---
+++ LeetCode/P16Medium_3Sum_Closest/main.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums = sorted(nums)
+        closest = sum(nums[:3])
+        for i in range(len(nums)-2):
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                s = sum((nums[i], nums[l], nums[r]))
+                if abs(s-target) < abs(closest-target):
+                    closest = s
+                if s < target:
+                    l += 1
+                elif s > target:
+                    r -= 1
+                else:
+                    return target
+        return closest
