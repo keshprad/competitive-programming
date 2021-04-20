45ac6f11ad304707c5877230f4b4674adc5e6223
keshprad <keshav.r.pradeep@gmail.com>
2021-04-19 17:33:01 -0700

    BFS solution for max depth of bin tree

--- LeetCode/P104Easy_Maximum_Depth_of_Binary_Tree/main.py
+++ LeetCode/P104Easy_Maximum_Depth_of_Binary_Tree/main.py
@@ -1,3 +1,4 @@
+from collections import deque
 # Problem: https://leetcode.com/problems/maximum-depth-of-binary-tree/
 
 
@@ -16,3 +17,16 @@ class Solution:
             return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
         else:
             return 0
+
+    # Breadth First Search
+    def maxDepth_BFS(self, root: TreeNode) -> int:
+        depth, level = 0, deque([root]) if root else deque()
+        while level:
+            depth += 1
+            for i in range(len(level)):
+                node = level.popleft()
+                if node.left:
+                    level.append(node.left)
+                if node.right:
+                    level.append(node.right)
+        return depth
