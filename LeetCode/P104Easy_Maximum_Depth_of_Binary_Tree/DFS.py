9e2747c97333e856e82bfdced42872b0bd2e8eee
keshprad <keshav.r.pradeep@gmail.com>
2021-04-19 17:11:26 -0700

    DFS solution for max depth of binary tree

---
+++ LeetCode/P104Easy_Maximum_Depth_of_Binary_Tree/DFS.py
@@ -0,0 +1,18 @@
+# Problem: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    # Depth First Search
+    def maxDepth_DFS(self, root: TreeNode) -> int:
+        if root:
+            return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        else:
+            return 0
