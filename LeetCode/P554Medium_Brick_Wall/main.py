cee6a5041235940183b1a2e388e1927a2c9a5535
keshprad <keshav.r.pradeep@gmail.com>
2021-04-21 14:49:35 -0700

    Solution to Leetcode Brick Wall problem

---
+++ LeetCode/P554Medium_Brick_Wall/main.py
@@ -0,0 +1,16 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        edges = defaultdict(int)
+        max_edges = 0
+
+        for layer in wall:
+            vert_line = 0
+            for brick in layer[:-1]:
+                vert_line += brick
+                edges[vert_line] += 1
+                max_edges = max(max_edges, edges[vert_line])
+        return len(wall) - max_edges
