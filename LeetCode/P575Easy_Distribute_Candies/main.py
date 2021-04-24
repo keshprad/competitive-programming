d2a17d1609be105ab69cc2bdd07b99386cf850ce
keshprad <keshav.r.pradeep@gmail.com>
2021-04-23 19:11:35 -0700

    1-liner Solution for Leet#575 Distribute Candies

---
+++ LeetCode/P575Easy_Distribute_Candies/main.py
@@ -0,0 +1,7 @@
+from typing import List
+# Problem: https://leetcode.com/problems/distribute-candies/
+
+
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int:
+        return min(len(set(candyType)), len(candyType)//2)
