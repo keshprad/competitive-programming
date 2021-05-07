d6d89342e176034bb50407834950ebaef135d261
keshprad <keshav.r.pradeep@gmail.com>
2021-05-07 15:12:27 -0700

    solution to Reverse linked list

---
+++ LeetCode/P206Easy_Reverse_Linked_List/main.py
@@ -0,0 +1,23 @@
+# Problem: https://leetcode.com/problems/reverse-linked-list/
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev = None
+        while head:
+            temp = head.next
+            head.next = prev
+            prev = head
+            head = temp
+        return prev
