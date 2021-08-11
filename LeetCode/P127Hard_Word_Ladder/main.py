e880756695729e8df349bd724812607b3c43dffa
keshprad <32313895+keshprad@users.noreply.github.com>
2021-08-11 00:38:19 -0700

    Add solution to Leet#127 Word Ladder

---
+++ LeetCode/P127Hard_Word_Ladder/main.py
@@ -0,0 +1,54 @@
+from collections import deque
+from typing import List
+# Problem: https://leetcode.com/problems/word-ladder/
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str,
+                     wordList: List[str]) -> int:
+        wordList = set(wordList)
+        alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        queue = deque([[beginWord, 1]])
+
+        while queue:
+            word, ladder_len = queue.popleft()
+
+            if word == endWord:
+                return ladder_len
+
+            for i in range(len(word)):
+                for letter in alphabet:
+                    next_word = f'{word[:i]}{letter}{word[i+1:]}'
+                    if next_word in wordList:
+                        queue.append([next_word, ladder_len + 1])
+                        # Remove next_word from wordList.
+                        # Sequence cannot include next_word later, since it would mean a valid shorter sequence exists.
+                        wordList.remove(next_word)
+        return 0
+
+    # Extending the problem to remember the ladder path as well as length
+    def ladderLength_track_ladder(self, beginWord: str, endWord: str,
+                                  wordList: List[str]) -> int:
+        wordList = set(wordList)
+        alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        queue = deque([[beginWord, beginWord, 1]])
+
+        while queue:
+            word, ladder, ladder_len = queue.popleft()
+
+            if word == endWord:
+                return ladder, ladder_len
+
+            for i in range(len(word)):
+                # Try all word combinations and check if in alphabet
+                for letter in alphabet:
+                    next_word = f'{word[:i]}{letter}{word[i+1:]}'
+                    if next_word in wordList:
+                        queue.append([
+                            next_word, f'{ladder} -> {next_word}',
+                            ladder_len + 1
+                        ])
+                        # Remove next_word from wordList.
+                        # Sequence cannot include next_word later, since it would mean a valid shorter sequence exists.
+                        wordList.remove(next_word)
+        return 0
