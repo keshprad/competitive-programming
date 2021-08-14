1287d7cbb2a237b6f11d3621cd1d66d20daaa91f
keshprad <32313895+keshprad@users.noreply.github.com>
2021-08-14 08:49:41 -0700

    Solution for Leet#62 - Unique Paths

---
+++ LeetCode/P62Medium_Unique_Paths/main.py
@@ -0,0 +1,19 @@
+# Problem: https://leetcode.com/problems/unique-paths/
+
+
+class Solution:
+    # Optimized approach using O(n) space & O(mn) time
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[j] += dp[j - 1]
+        return dp[-1]
+
+    # Straightforward DP soltution using O(mn) space and time
+    def uniquePaths2(self, m: int, n: int) -> int:
+        dp = [[1] * n for _ in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[-1][-1]
