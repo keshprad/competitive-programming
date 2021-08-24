8dbb55bd47de0305f7304e4e7ccbbac5d0baecb8
keshprad <32313895+keshprad@users.noreply.github.com>
2021-08-23 22:55:41 -0400

    Add a recursive solution

--- LeetCode/P67Easy_Add_Binary/main.py
+++ LeetCode/P67Easy_Add_Binary/main.py
@@ -22,3 +22,21 @@ class Solution:
 
         # Reverse order since binary sum was calculated & stored in reverse order
         return bin_sum[::-1]
+
+    # Recursive solution
+    def addBinary2(self, a: str, b: str) -> str:
+        # Base cases to return after one list is empty
+        if not a: return b
+        if not b: return a
+
+        # Compare last element to determine recursive case
+        if a[-1] == '0' and b[-1] == '0':
+            # binary sum = '0' at last index; recursively call on all but last index.
+            return self.addBinary2(a[:-1], b[:-1]) + '0'
+        elif a[-1] == '1' and b[-1] == '1':
+            # binary sum = '10' at last index; recursively call on all but last index.
+            # In order to carry the '1', make another recursive call to add '1' to current result
+            return self.addBinary2(self.addBinary2(a[:-1], b[:-1]), '1') + '0'
+        else:
+            # binary sum = '1' at last index; recursively call on all but last index.
+            return self.addBinary2(a[:-1], b[:-1]) + '1'
