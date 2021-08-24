48de3c21a41c08514023c2cadbaf22df84c97628
keshprad <32313895+keshprad@users.noreply.github.com>
2021-08-23 22:02:47 -0400

    Add code comment

--- LeetCode/P67Easy_Add_Binary/main.py
+++ LeetCode/P67Easy_Add_Binary/main.py
@@ -2,18 +2,23 @@
 
 
 class Solution:
+    # Iterative Solution
     def addBinary(self, a: str, b: str) -> str:
         bin_sum = ''
         a, b = map(list, (a, b))
 
         carry = 0
         while a or b:
+            # Iterate through each index in reverse order
             num1 = int(a.pop()) if a else 0
             num2 = int(b.pop()) if b else 0
 
-            carry, rem = divmod(num1 + num2 + carry, 2)
-            bin_sum += str(rem)
+            # Calculate a carry and a val for the current index
+            carry, val = divmod(num1 + num2 + carry, 2)
+            bin_sum += str(val)
 
+        # If a carry is leftover from the loop, add to bin_sum
         bin_sum += str(carry) if carry else ''
 
+        # Reverse order since binary sum was calculated & stored in reverse order
         return bin_sum[::-1]
