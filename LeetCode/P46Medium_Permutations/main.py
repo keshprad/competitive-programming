bc100ac6b8a3b5d1f010f84aa5b774883f1802b3
keshprad <32313895+keshprad@users.noreply.github.com>
2021-09-10 11:23:09 -0400

    formatting for Leet#46

--- LeetCode/P46Medium_Permutations/main.py
+++ LeetCode/P46Medium_Permutations/main.py
@@ -8,16 +8,11 @@ class Solution:
         self.permute_dfs(nums, [], permutations)
         return permutations
 
-    def permute_dfs(self, nums: List[int], path: List[int], permutations: List[List[int]]) -> List[List[int]]:
+    # DFS helper for permute
+    def permute_dfs(self, nums: List[int], path: List[int], permutations: List[List[int]]):
         if not nums:
             permutations.append(path)
         else:
             for i in range(len(nums)):
                 self.permute_dfs(nums[:i]+nums[i+1:],
                                  path+[nums[i]], permutations)
-
-
-sol = Solution()
-print(sol.permute([1, 2, 3]))
-print(sol.permute([0, 1]))
-print(sol.permute([1]))
