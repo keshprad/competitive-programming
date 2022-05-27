6619bf373cfc2a97218ebbff7e1c7b27edc96f0f
keshprad <32313895+keshprad@users.noreply.github.com>
2022-05-27 15:47:01 -0700

    Leet#15: comment solution

--- LeetCode/P15Medium_3Sum/main.py
+++ LeetCode/P15Medium_3Sum/main.py
@@ -4,23 +4,35 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res, nums = [], sorted(nums)
-        for i in range(len(nums)-2):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                total = nums[i] + nums[l] + nums[r]
-                if total < 0:
-                    l += 1
-                elif total > 0:
-                    r -= 1
-                else:
-                    res.append([nums[i], nums[l], nums[r]])
-                    while l < r and nums[l] == nums[l+1]:
+        nums.sort()
+        res = []
+
+        for i, num in enumerate(nums):
+            # skip dup nums[i] vals
+            if i == 0 or nums[i - 1] != nums[i]:
+                l, r = i + 1, len(nums) - 1
+
+                while l < r:
+                    total = num + nums[l] + nums[r]
+                    if total < 0:
                         l += 1
-                    while l < r and nums[r] == nums[r-1]:
+                    elif total > 0:
                         r -= 1
-                    l += 1
-                    r -= 1
+                    else:
+                        # Found one solution.
+                        # this nums[i] value can still have more solutions
+                        res.append([nums[i], nums[l], nums[r]])
+
+                        # skip dup nums[l] vals
+                        while l < r and nums[l] == nums[l + 1]:
+                            l += 1
+                        # inc 1 more after to get the new val
+                        l += 1
+
+                        # skip dup nums[r] vals
+                        while l < r and nums[r] == nums[r - 1]:
+                            r -= 1
+                        # dec 1 more after to get the new val
+                        r -= 1
+
         return res
