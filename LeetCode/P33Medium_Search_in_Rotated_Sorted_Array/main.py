3782fa1bde85529ede146f080f589125853b26f2
keshprad <32313895+keshprad@users.noreply.github.com>
2022-05-27 15:32:31 -0700

    Revisit solution with comments

--- LeetCode/P33Medium_Search_in_Rotated_Sorted_Array/main.py
+++ LeetCode/P33Medium_Search_in_Rotated_Sorted_Array/main.py
@@ -12,13 +12,25 @@ class Solution:
                 return mid
 
             if nums[mid] <= nums[r]:
+                # nums is increasing from mid to r.
+                # the partition is somewhere between l and mid
+
                 if nums[mid] < target <= nums[r]:
+                    # if target in nums, it's in right half
                     l = mid + 1
                 else:
+                    # if target in nums, it's in left half
                     r = mid - 1
             else:
+                # nums is increasing from l to mid
+                # the partition is somewhere between mid and r
+
                 if nums[l] <= target < nums[mid]:
+                    # if target in nums, its in left half
                     r = mid - 1
                 else:
+                    # if target in nums, it's in right half
                     l = mid + 1
+
+        # target not found
         return -1
