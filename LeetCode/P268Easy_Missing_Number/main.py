e9c919e624c3904ee879b4e4e49846d531be837a
keshprad <32313895+keshprad@users.noreply.github.com>
2022-05-30 22:50:07 -0700

    Solution for Leet#268

--- LeetCode/P268Easy_Missing_Number/main.py
+++ LeetCode/P268Easy_Missing_Number/main.py
@@ -4,10 +4,19 @@ from typing import List
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        '''O(n) runtime, O(1) space solution using XOR'''
-        # a ^ a = 0
-        # a ^ 0 = a
-        # a ^ b = b ^ a
+        '''O(n) runtime, O(1) space solution using XOR
+
+        In order to understand this solution, these key properties of XOR
+        should be known.
+        a ^ a = 0
+        a ^ 0 = a
+        a ^ b = b ^ a
+
+        Using these properties we see that XOR'ing the list of nums with all
+        the nums from 0 to n, will allow all matching a's to XOR with each
+        other (a ^ a = 0). This would leave 0 ^ (the last num, which doesn't
+        exist in nums). This will equal itself.
+        '''
         res = 0
 
         for i, num in enumerate(nums):
