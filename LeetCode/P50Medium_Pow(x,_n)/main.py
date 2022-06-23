b6abcccf9050877dc393287247970590a96598e6
keshprad <32313895+keshprad@users.noreply.github.com>
2022-06-23 12:11:10 -0700

    Leet#50 solution

--- LeetCode/P50Medium_Pow(x,_n)/main.py
+++ LeetCode/P50Medium_Pow(x,_n)/main.py
@@ -8,6 +8,6 @@ class Solution:
         elif n < 0:
             return 1 / self.myPow(x, -n)
         elif n % 2 == 0:
-            return self.myPow(x*x, n // 2)
+            return self.myPow(x * x, n // 2)
         else:
             return x * self.myPow(x, n - 1)
