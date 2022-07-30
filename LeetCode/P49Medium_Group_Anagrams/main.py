8ce8ec029df6562721094e1418347fd6d84b1a10
keshprad <32313895+keshprad@users.noreply.github.com>
2022-07-30 15:09:08 -0700

    Solution for leet 49

--- LeetCode/P49Medium_Group_Anagrams/main.py
+++ LeetCode/P49Medium_Group_Anagrams/main.py
@@ -1,11 +1,15 @@
+from collections import defaultdict
 from typing import List
 # Problem: https://leetcode.com/problems/group-anagrams/
 
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = {}
+        anagrams = defaultdict(list)
+
         for word in strs:
+            # all anagrams will have the same key
             key = ''.join(sorted(word))
-            anagrams[key] = anagrams.get(key, []) + [word]
+            anagrams[key].append(word)
+
         return list(anagrams.values())
