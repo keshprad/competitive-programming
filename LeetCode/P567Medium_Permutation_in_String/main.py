e832ac59e51264c96e121db4e44f8142bee48f4f
keshprad <32313895+keshprad@users.noreply.github.com>
2022-08-23 11:41:24 -0700

    Sol for Leet #567

--- LeetCode/P567Medium_Permutation_in_String/main.py
+++ LeetCode/P567Medium_Permutation_in_String/main.py
@@ -4,6 +4,44 @@ import string
 
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        '''O(n) time, O(26) space solution'''
+        if len(s1) > len(s2):
+            return False
+
+        # find init counts of window
+        s1_count = [0] * 26
+        s2_count = [0] * 26
+        for i in range(len(s1)):
+            s1_count[ord(s1[i]) - ord('a')] += 1
+            s2_count[ord(s2[i]) - ord('a')] += 1
+
+        # find init matches of window
+        matches = 0
+        for i in range(26):
+            matches += s1_count[i] == s2_count[i]
+
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+
+            char = ord(s2[r]) - ord('a')
+            s2_count[char] += 1
+            if s1_count[char] == s2_count[char]:
+                matches += 1
+            elif s1_count[char] + 1 == s2_count[char]:
+                matches -= 1
+
+            char = ord(s2[l]) - ord('a')
+            s2_count[char] -= 1
+            if s1_count[char] == s2_count[char]:
+                matches += 1
+            elif s1_count[char] - 1 == s2_count[char]:
+                matches -= 1
+            l += 1
+        return matches == 26
+
+    def checkInclusion2(self, s1: str, s2: str) -> bool:
         '''O(26n) time, O(26) space solution'''
         def get_counts(s: str) -> dict:
             d = {c: 0 for c in string.ascii_lowercase}
