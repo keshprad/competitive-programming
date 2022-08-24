413821efc7cee4b6e9ef65996157bca5bfc162e0
keshprad <32313895+keshprad@users.noreply.github.com>
2022-08-24 09:09:05 -0700

    Solution for Leet#150

--- LeetCode/P150Medium_Evaluate_Reverse_Polish_Notation/main.py
+++ LeetCode/P150Medium_Evaluate_Reverse_Polish_Notation/main.py
@@ -4,24 +4,19 @@ from collections import deque
 
 
 class Solution:
-    def add(self, a, b): return a + b
-
-    def diff(self, a, b): return a - b
-
-    def prod(self, a, b): return a * b
-
-    def quotient(self, a, b): return int(a / b)
-
     def evalRPN(self, tokens: List[str]) -> int:
-        operators = {'+': self.add, '-': self.diff,
-                     '*': self.prod, '/': self.quotient}
-        stack = deque()
+        stack = []
+        ops = {'+': lambda a, b: a + b,
+               '-': lambda a, b: a - b,
+               '*': lambda a, b: a * b,
+               '/': lambda a, b: int(a / b)}
 
         for token in tokens:
-            if token in operators:
+            if token in ops:
                 b, a = stack.pop(), stack.pop()
-                c = operators[token](a, b)
-                stack.append(c)
+                res = ops[token](a, b)
+                stack.append(res)
             else:
                 stack.append(int(token))
+
         return stack.pop()
