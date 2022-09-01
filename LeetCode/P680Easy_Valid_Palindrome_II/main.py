28b0089c08e4c9bcfd9a5f5c941cd4b940d2c549
keshprad <32313895+keshprad@users.noreply.github.com>
2022-08-31 21:56:52 -0400

    Sol for Leet#680

--- LeetCode/P680Easy_Valid_Palindrome_II/main.py
+++ LeetCode/P680Easy_Valid_Palindrome_II/main.py
@@ -2,7 +2,7 @@
 
 
 class Solution:
-    def validPalindrome2(self, s: str) -> bool:
+    def validPalindrome(self, s: str) -> bool:
         def isPalindrome(s: str) -> bool:
             l, r = 0, len(s) - 1
 
@@ -18,22 +18,8 @@ class Solution:
 
         while l < r:
             if s[l] != s[r]:
-                return isPalindrome(s[:l] + s[l+1:]) or isPalindrome(s[:r] + s[r+1:])
+                return isPalindrome(s[l:r]) or isPalindrome(s[l+1:r+1])
 
             l += 1
             r -= 1
         return True
-
-    def validPalindrome(self, s: str) -> bool:
-        def isPalindrome(s: str, fail_expr=lambda l, r: False) -> bool:
-            l, r = 0, len(s) - 1
-
-            while l < r:
-                if s[l] != s[r]:
-                    return fail_expr(l, r)
-
-                l += 1
-                r -= 1
-            return True
-
-        return isPalindrome(s, lambda l, r: isPalindrome(s[:l] + s[l+1:]) or isPalindrome(s[:r] + s[r+1:]))
