de00531a5fa1fd35fb2360a3e24e20c85f367c72
keshprad <32313895+keshprad@users.noreply.github.com>
2022-10-11 18:38:01 -0400

    Sol for leet 4

--- LeetCode/P4Hard_Median_of_Two_Sorted_Arrays/main.py
+++ LeetCode/P4Hard_Median_of_Two_Sorted_Arrays/main.py
@@ -4,6 +4,39 @@ from typing import List
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
+        half = total // 2
+
+        if len(A) > len(B):
+            B, A = A, B
+
+        l = 0
+        r = len(A) - 1
+        while True:
+            i = l + (r - l) // 2    # A
+            j = half - i - 2        # B
+
+            Aleft = A[i] if i >= 0 else float('-inf')
+            Aright = A[i+1] if i + 1 < len(A) else float('inf')
+
+            Bleft = B[j] if j >= 0 else float('-inf')
+            Bright = B[j+1] if j + 1 < len(B) else float('inf')
+
+            if Aleft <= Bright and Bleft <= Aright:
+                # partition is valid
+                if total % 2 == 1:
+                    # odd
+                    return min(Aright, Bright)
+                else:
+                    # even
+                    return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+            elif Aleft > Bright:
+                r = i - 1
+            else:
+                l = i + 1
+
+    def findMedianSortedArrays2(self, nums1: List[int], nums2: List[int]) -> float:
         merged = sorted(nums1 + nums2)
         if len(merged) == 0:
             return 0.0
