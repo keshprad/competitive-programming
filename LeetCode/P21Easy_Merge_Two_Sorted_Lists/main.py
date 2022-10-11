adf936566973bb4791dddb55d2004b76c937a569
keshprad <32313895+keshprad@users.noreply.github.com>
2022-10-11 19:10:27 -0400

    iterative sol for leet 21

--- LeetCode/P21Easy_Merge_Two_Sorted_Lists/main.py
+++ LeetCode/P21Easy_Merge_Two_Sorted_Lists/main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 # Problem: https://leetcode.com/problems/merge-two-sorted-lists/
 
 
@@ -15,3 +16,23 @@ class Solution:
         if l1:
             l1.next = self.mergeTwoLists(l1.next, l2)
         return l1
+
+    def mergeTwoLists2(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        curr = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+
+        if list1:
+            curr.next = list1
+        else:
+            curr.next = list2
+
+        return dummy.next
