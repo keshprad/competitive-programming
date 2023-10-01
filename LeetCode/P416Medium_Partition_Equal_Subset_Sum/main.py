c733559d93d40fc6c5f0c2cb11bc814615b0724d
keshprad <32313895+keshprad@users.noreply.github.com>
2023-10-01 10:59:09 -0400

    sol for p416

--- LeetCode/P416Medium_Partition_Equal_Subset_Sum/main.py
+++ LeetCode/P416Medium_Partition_Equal_Subset_Sum/main.py
@@ -1,5 +1,4 @@
 from typing import List
-import numpy as np
 # Problem: https://leetcode.com/problems/partition-equal-subset-sum/
 
 
@@ -8,13 +7,14 @@ class Solution:
         total = sum(nums)
         if total % 2 == 1:
             return False
-        total = total // 2
+        target = total // 2
 
-        bag = np.full(shape=total+1, fill_value=False)
-        bag[0] = True
+        # set up dp matrix
+        dp = [[False]*(target+1) for _ in range(len(nums)+1)]
+        dp[0][0] = True
 
-        for num in nums:
-            for i in range(total, 0, -1):
-                if i >= num and bag[i-num]:
-                    bag[i] = True
-        return bag[total]
+        for i in range(1, len(nums)+1):
+            num = nums[i-1]
+            for j in range(target+1):
+                dp[i][j] = dp[i-1][j] or (num <= j and dp[i-1][j-num])
+        return dp[-1][-1]
