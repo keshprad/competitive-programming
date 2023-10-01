46b80dca7f3eca8a16ab10ec2ff5cf6c21b622a0
keshprad <32313895+keshprad@users.noreply.github.com>
2023-10-01 15:10:13 -0400

    sol for leet5

--- LeetCode/P5Medium_Longest_Palindromic_Substring/main.py
+++ LeetCode/P5Medium_Longest_Palindromic_Substring/main.py
@@ -3,13 +3,37 @@
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        pal = ''
+        # initial longest palindrome
+        best = '' if not s else s[-1]
+        # memo indicating whether substr spanning (i, j) is a palindrome
+        # set all substrs of len 1 to palindromes
+        dp = {(i, i): True for i in range(len(s))}
 
-        for l in range(len(s)):
-            for r in range(len(s), l, -1):
-                # Check if the length is longer than the current palindrome
-                # Check if it is a palindrome from index l to r
-                if r - l > len(pal) and s[l:r] == s[l:r][::-1]:
-                    pal = s[l:r]
+        for m in range(0, len(s)):
+            # m is the middle of our palindrome
+
+            l, r = m-1, m+1
+
+            # if palindrome length not odd, our midpoint could be
+            # an even amt of letters. Expand to include duplicates
+            while l >= 0 and s[l] == s[m]:
+                l -= 1
+            while r < len(s) and s[r] == s[m]:
+                r += 1
+            dp[(l+1, r-1)] = True
+
+            while l >= 0 and r < len(s):
+                # expand outward (move l left and r right)
+                dp[(l, r)] = dp[(l+1, r-1)] and s[l] == s[r]
+
+                # no longer a palindrome
+                if not dp[(l, r)]:
                     break
-        return pal
+
+                l -= 1
+                r += 1
+
+            if len(best) < r - l - 1:
+                best = s[l+1:r]
+
+        return best
