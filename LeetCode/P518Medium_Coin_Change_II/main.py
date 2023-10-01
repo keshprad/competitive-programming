5b3abb369d929ab5df5d60258414f69e91d65763
keshprad <32313895+keshprad@users.noreply.github.com>
2023-10-01 10:37:36 -0400

    sol for p518

---
+++ LeetCode/P518Medium_Coin_Change_II/main.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+from typing import List
+# Problem: https://leetcode.com/problems/coin-change-ii/
+
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [[0]*(amount+1) for _ in range(len(coins)+1)]
+        for i in range(len(coins)+1):
+            dp[i][0] = 1
+
+        for i in range(1, len(coins)+1):
+            coin = coins[i-1]
+            for j in range(1, amount+1):
+                dp[i][j] = dp[i-1][j] + (dp[i][j-coin] if j >= coin else 0)
+        return dp[-1][-1]
