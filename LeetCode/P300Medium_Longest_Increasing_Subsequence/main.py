f3e67ba440ef586753ca19c14a64d0de465a3f2c
keshprad <32313895+keshprad@users.noreply.github.com>
2023-11-05 16:15:02 -0500

    2nd sol for p300

--- LeetCode/P300Medium_Longest_Increasing_Subsequence/main.py
+++ LeetCode/P300Medium_Longest_Increasing_Subsequence/main.py
@@ -1,9 +1,25 @@
 from typing import List
+from bisect import bisect_left
 # Problem: https://leetcode.com/problems/longest-increasing-subsequence/
 
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        # start array with first elem
+        arr = [nums.pop(0)]
+
+        for num in nums:
+            if num > arr[-1]:
+                # case 1: append elem to arr
+                arr.append(num)
+            else:
+                # case 2: can't append elem to arr
+                # instead, replace closest number greater
+                arr[bisect_left(arr, num)] = num
+        return len(arr)
+
+    
+    def lengthOfLIS2(self, nums: List[int]) -> int:
         dp = {}
 
         for i in range(len(nums)-1, -1, -1):
